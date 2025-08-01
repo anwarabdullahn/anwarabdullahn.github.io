88d6ea92d8fb23d6880443bf49d0c0494631de7e
Anwar Abdullah <anwarabdullahn28@gmail.com>
2025-08-02 01:16:51 +0700

    refactor sections

---
+++ src/components/sections/ServicesSection.tsx
@@ -0,0 +1,189 @@
+import React from 'react';
+
+interface ServicesSectionProps {
+  className?: string;
+}
+
+const ServicesSection: React.FC<ServicesSectionProps> = ({ className }) => {
+  return (
+    <div className={`elisc_tm_section ${className || ''}`} id="service">
+      <div className="elisc_tm_services">
+        <div className="tm_content">
+          <div className="elisc_tm_service_title">
+            <div className="elisc_tm_title">
+              <span>- Services</span>
+              <h3>My Services</h3>
+            </div>
+            <a href="mailto:aannwaran@gmail.com">aannwaran@gmail.com</a>
+          </div>
+          <div className="service_list">
+            <ul>
+              <li>
+                <img className="popup_image" src="/assets/images/service/2.jpg" alt="" />
+                <div className="list_inner">
+                  <div className="details">
+                    <div className="title">
+                      <span>01</span>
+                      <h3>Web Design</h3>
+                    </div>
+                    <div className="text">
+                      <p>Web development is the process of building, programming...</p>
+                    </div>
+                    <div className="elisc_tm_read_more">
+                      <a href="#">Read More<span><img className="svg" src="/assets/images/svg/rightArrow.svg" alt="" /></span></a>
+                    </div>
+                  </div>
+                  <a className="elisc_tm_full_link" href="#"></a>
+                  <div className="hidden_details">
+                    <div className="descriptions">
+                      <p>Elisc is a leading web design agency with an award-winning design team that creates innovative, effective websites that capture your brand, improve your conversion rates, and maximize your revenue to help grow your business and achieve your goals.</p>
+                      <p>In today&apos;s digital world, your website is the first interaction consumers have with your business. That&apos;s why almost 95 percent of a user&apos;s first impression relates to web design. It&apos;s also why web design services can have an immense impact on your company&apos;s bottom line.</p>
+                      <p>That&apos;s why more companies are not only reevaluating their website&apos;s design but also partnering with Elisc, the web design agency that&apos;s driven more than $2.4 billion in revenue for its clients. With over 50 web design awards under our belt, we&apos;re confident we can design a custom website that drives sales for your unique business.</p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <img className="popup_image" src="/assets/images/service/3.jpg" alt="" />
+                <div className="list_inner">
+                  <div className="details">
+                    <div className="title">
+                      <span>02</span>
+                      <h3>UI/UX Design</h3>
+                    </div>
+                    <div className="text">
+                      <p>Web development is the process of building, programming...</p>
+                    </div>
+                    <div className="elisc_tm_read_more">
+                      <a href="#">Read More<span><img className="svg" src="/assets/images/svg/rightArrow.svg" alt="" /></span></a>
+                    </div>
+                  </div>
+                  <a className="elisc_tm_full_link" href="#"></a>
+                  <div className="hidden_details">
+                    <div className="descriptions">
+                      <p>Elisc is a leading web design agency with an award-winning design team that creates innovative, effective websites that capture your brand, improve your conversion rates, and maximize your revenue to help grow your business and achieve your goals.</p>
+                      <p>In today&apos;s digital world, your website is the first interaction consumers have with your business. That&apos;s why almost 95 percent of a user&apos;s first impression relates to web design. It&apos;s also why web design services can have an immense impact on your company&apos;s bottom line.</p>
+                      <p>That&apos;s why more companies are not only reevaluating their website&apos;s design but also partnering with Elisc, the web design agency that&apos;s driven more than $2.4 billion in revenue for its clients. With over 50 web design awards under our belt, we&apos;re confident we can design a custom website that drives sales for your unique business.</p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <img className="popup_image" src="/assets/images/service/4.jpg" alt="" />
+                <div className="list_inner">
+                  <div className="details">
+                    <div className="title">
+                      <span>03</span>
+                      <h3>Mobile Application</h3>
+                    </div>
+                    <div className="text">
+                      <p>Web development is the process of building, programming...</p>
+                    </div>
+                    <div className="elisc_tm_read_more">
+                      <a href="#">Read More<span><img className="svg" src="/assets/images/svg/rightArrow.svg" alt="" /></span></a>
+                    </div>
+                  </div>
+                  <a className="elisc_tm_full_link" href="#"></a>
+                  <div className="hidden_details">
+                    <div className="descriptions">
+                      <p>Elisc is a leading web design agency with an award-winning design team that creates innovative, effective websites that capture your brand, improve your conversion rates, and maximize your revenue to help grow your business and achieve your goals.</p>
+                      <p>In today&apos;s digital world, your website is the first interaction consumers have with your business. That&apos;s why almost 95 percent of a user&apos;s first impression relates to web design. It&apos;s also why web design services can have an immense impact on your company&apos;s bottom line.</p>
+                      <p>That&apos;s why more companies are not only reevaluating their website&apos;s design but also partnering with Elisc, the web design agency that&apos;s driven more than $2.4 billion in revenue for its clients. With over 50 web design awards under our belt, we&apos;re confident we can design a custom website that drives sales for your unique business.</p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <img className="popup_image" src="/assets/images/service/5.jpg" alt="" />
+                <div className="list_inner">
+                  <div className="details">
+                    <div className="title">
+                      <span>04</span>
+                      <h3>User Research</h3>
+                    </div>
+                    <div className="text">
+                      <p>Web development is the process of building, programming...</p>
+                    </div>
+                    <div className="elisc_tm_read_more">
+                      <a href="#">Read More<span><img className="svg" src="/assets/images/svg/rightArrow.svg" alt="" /></span></a>
+                    </div>
+                  </div>
+                  <a className="elisc_tm_full_link" href="#"></a>
+                  <div className="hidden_details">
+                    <div className="descriptions">
+                      <p>Elisc is a leading web design agency with an award-winning design team that creates innovative, effective websites that capture your brand, improve your conversion rates, and maximize your revenue to help grow your business and achieve your goals.</p>
+                      <p>In today&apos;s digital world, your website is the first interaction consumers have with your business. That&apos;s why almost 95 percent of a user&apos;s first impression relates to web design. It&apos;s also why web design services can have an immense impact on your company&apos;s bottom line.</p>
+                      <p>That&apos;s why more companies are not only reevaluating their website&apos;s design but also partnering with Elisc, the web design agency that&apos;s driven more than $2.4 billion in revenue for its clients. With over 50 web design awards under our belt, we&apos;re confident we can design a custom website that drives sales for your unique business.</p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <img className="popup_image" src="/assets/images/service/6.jpg" alt="" />
+                <div className="list_inner">
+                  <div className="details">
+                    <div className="title">
+                      <span>05</span>
+                      <h3>Animation</h3>
+                    </div>
+                    <div className="text">
+                      <p>Web development is the process of building, programming...</p>
+                    </div>
+                    <div className="elisc_tm_read_more">
+                      <a href="#">Read More<span><img className="svg" src="/assets/images/svg/rightArrow.svg" alt="" /></span></a>
+                    </div>
+                  </div>
+                  <a className="elisc_tm_full_link" href="#"></a>
+                  <div className="hidden_details">
+                    <div className="descriptions">
+                      <p>Elisc is a leading web design agency with an award-winning design team that creates innovative, effective websites that capture your brand, improve your conversion rates, and maximize your revenue to help grow your business and achieve your goals.</p>
+                      <p>In today&apos;s digital world, your website is the first interaction consumers have with your business. That&apos;s why almost 95 percent of a user&apos;s first impression relates to web design. It&apos;s also why web design services can have an immense impact on your company&apos;s bottom line.</p>
+                      <p>That&apos;s why more companies are not only reevaluating their website&apos;s design but also partnering with Elisc, the web design agency that&apos;s driven more than $2.4 billion in revenue for its clients. With over 50 web design awards under our belt, we&apos;re confident we can design a custom website that drives sales for your unique business.</p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <img className="popup_image" src="/assets/images/service/7.jpg" alt="" />
+                <div className="list_inner">
+                  <div className="details">
+                    <div className="title">
+                      <span>06</span>
+                      <h3>Game Development</h3>
+                    </div>
+                    <div className="text">
+                      <p>Web development is the process of building, programming...</p>
+                    </div>
+                    <div className="elisc_tm_read_more">
+                      <a href="#">Read More<span><img className="svg" src="/assets/images/svg/rightArrow.svg" alt="" /></span></a>
+                    </div>
+                  </div>
+                  <a className="elisc_tm_full_link" href="#"></a>
+                  <div className="hidden_details">
+                    <div className="descriptions">
+                      <p>Elisc is a leading web design agency with an award-winning design team that creates innovative, effective websites that capture your brand, improve your conversion rates, and maximize your revenue to help grow your business and achieve your goals.</p>
+                      <p>In today&apos;s digital world, your website is the first interaction consumers have with your business. That&apos;s why almost 95 percent of a user&apos;s first impression relates to web design. It&apos;s also why web design services can have an immense impact on your company&apos;s bottom line.</p>
+                      <p>That&apos;s why more companies are not only reevaluating their website&apos;s design but also partnering with Elisc, the web design agency that&apos;s driven more than $2.4 billion in revenue for its clients. With over 50 web design awards under our belt, we&apos;re confident we can design a custom website that drives sales for your unique business.</p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div className="elisc_tm_video">
+            <img className="placeholder" src="/assets/images/thumbs/4-2.jpg" alt="" />
+            <div className="image" data-img-url="/assets/images/service/1.jpg"></div>
+            <div className="overlay"></div>
+            <span className="play"><img className="svg" src="/assets/images/svg/play.svg" alt="" /></span>
+            <div className="text">
+              <h3>Intro Video</h3>
+            </div>
+            <a className="elisc_tm_full_link popup-youtube" href="https://www.youtube.com/watch?v=7e90gBu4pas" onClick={(e) => { e.preventDefault(); window.open('https://www.youtube.com/watch?v=7e90gBu4pas', '_blank'); }}></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServicesSection;
